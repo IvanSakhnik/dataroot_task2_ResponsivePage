26970cb599bdd02631f4c8280a9023d0a772a1d1
Viktoriia Brykalova <viktorytori1810@gmail.com>
2017-04-30 17:22:53 +0300

    work with slider

--- js/slider.js
+++ js/slider.js
@@ -1,68 +1,76 @@
 
+
 (function ( $ ) {
 
-	$.fn.slider = function( options ) {
+    $.fn.slider = function( options ) {
 
-    	var settings = $.extend ({
-    		infinite : true,
-    		index : 0,
-    		sliderVisible : 1,
-    		slidesToScroll : 1,
-    		arrows : true,
-    		swipeAble : true,
-    		pagination : true,
-    		autoplay : true,
-    		autoplaySpeed : 5000
-    	}, options);
+        var settings = $.extend ({
+            infinite : true,
+            index : 0,
+            sliderVisible : 6,
+            slidesToScroll : 1,
+            arrows : true,
+            swipeAble : true,
+            pagination : true,
+            autoplay : true,
+            autoplaySpeed : 2000
+        }, options);
 
-    	var make = function(){
-    		$('.slider').css({'owerflow':'hidden'})
-            //$('.slide').css({'position':'relative'})
-                           
-            var slideNum=0;
-            var slideTime;
-            var slideCount=$('.slide').size;
-                var animSlide=function(arrow){
-                var step = $('.slide').outerWidth();
-                if (arrow=='next'){
-                    $('.slide').eq(0).animate({'margin-left': -step}, 'slow', function(){
-                        //$('.slide').eq(0).css({'margin-left':'0'})
-                        $('.slide').eq(0).appendTo('.slider');
-                        //$('.slider').css('left',0)
-                    });
-                rotator();
-                };
 
-                /*$('.slide').eq(slideNum).fadeOut(settings.autoplaySpeed);
-                if (arrow=='next'){
-                    if (slideNum==(slideCount-1)){slideNum=0;}
-                    else{slideNum++}
-                }
-                else if (arrow == 'prew'){
-                    if (slideNum ==0){slideNum=slideCount-1;}
-                    else{slideNum-=1}
-                }
-                else{
-                    slideNum=arrow;
-                }*/
-                //$('.slide').eq(slideNum).fadeIn(settings.autoplaySpeed, rotator);
+
+        var slideCount= $('.slides-wrapper >.slide').length;
+        if (settings.sliderVisible>slideCount){
+            settings.sliderVisible=slideCount;
+        }
+        var step = $('.slide').outerWidth(true)
+        var slideCount= $('.slides-wrapper >.slide').length;
+        $('.slides-wrapper').css({'position':'relative'})
+        $('.slider').css({'width':step*settings.sliderVisible+settings.sliderVisible*2})
+        $('.slides-wrapper').css({'width':step*settings.sliderVisible+step+settings.sliderVisible*6})
+
+
+        var animateSlide = function(){
+            if ($('.slider').is(':hover')){
+                clearInterval(timer);
             }
-
-            var pause=false;
-            var rotator=function(){
-                if (!pause){slideTime=setTimeout(function(){animSlide('next')}, settings.autoplaySpeed);}
-                }
-
-            $('#slider-wrap').hover(    
-                function(){clearTimeout(slideTime); pause = true;},
-                function(){pause = false; rotator();
+            $('.slides-wrapper').animate({'left':-step}, 1000, 
+                function(){
+                    $('.slide').eq(0).remove();
+                    $('.slides-wrapper').css({'left':0});
+                    $('.slide').eq(0).clone().appendTo('.slides-wrapper');
                 });
-            rotator();
-            }
-    	
+        };
 
-    	return this.each(make);
-	};
+        if (settings.arrows == true) {
+            var $linkArrow = $('<a id="prewbutton" href="#">prew</a><a id="nextbutton" href="#">next</a>').prependTo('#slider-wrap');
+            //$('#prewbutton').css({'display':'block'});      
+            // $('#nextbutton').click(function(){
+            //     animateSlide("next");
+            // });
+            // $('#prewbutton').click(function(){
+            //     animateSlide("prew");
+            // });
+        }
+
+        var rotator = function(){
+            if (settings.autoplay==true){
+                var timer;
+                $('.slide').eq(0).clone().appendTo('.slides-wrapper');
+                if (settings.infinite==true){
+                    timer=setInterval(function(){
+                        animateSlide()
+
+                    }, settings.autoplaySpeed);
+                }
+            }
+        }
+
+        var make = function(){
+            rotator();
+        };
+        
+        return this.each(make);
+    };
 
 }( jQuery ));
  
@@ -70,5 +78,5 @@
 
 
 $(document).ready(function(){
-	$('.slider-wrap').slider();
+    $('.slider-wrap').slider();
 });
